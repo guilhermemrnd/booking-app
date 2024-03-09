5598c445389b7479dc39d8ea41a0bd5282524444
Guilherme Miranda <guilherme_de_oliveira_n1@hotmail.com>
2024-03-09 17:05:52 -0300

    feat (frontend): save user login state

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,19 +1,30 @@
 import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
 
 export default function Header() {
+  const { isLoggedIn } = useAppContext();
+
   return (
     <div className="bg-blue-800 py-6">
       <div className="container mx-auto flex justify-between">
-        <span className="text-2xl md:text-3xl font-bold tracking-tight text-white">
+        <span className="text-2xl font-bold tracking-tight text-white md:text-3xl">
           <Link to="/">BookingClone.com</Link>
         </span>
         <span className="flex space-x-2">
-          <Link
-            to="/"
-            className="flex bg-white cursor-pointer items-center px-3 font-bold text-blue-600 hover:bg-gray-100"
-          >
-            Sign In
-          </Link>
+          {isLoggedIn ? (
+            <>
+              <Link to="/my-bookings">My Bookings</Link>
+              <Link to="/my-hotels">My Hotels</Link>
+              <button>Sign out</button>
+            </>
+          ) : (
+            <Link
+              to="/register"
+              className="flex cursor-pointer items-center bg-white px-3 font-bold text-blue-600 hover:bg-gray-100"
+            >
+              Sign In
+            </Link>
+          )}
         </span>
       </div>
     </div>
--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,4 +1,7 @@
 import React, { useContext, useState } from "react";
+import { useQuery } from "react-query";
+
+import * as apiClient from "../api-client";
 import Toast from "../components/Toast";
 
 type ToastMessage = {
@@ -8,6 +11,7 @@ type ToastMessage = {
 
 type AppContextValue = {
   showToast: (toastMessage: ToastMessage) => void;
+  isLoggedIn: boolean;
 };
 
 const AppContext = React.createContext<AppContextValue | undefined>(undefined);
@@ -15,10 +19,13 @@ const AppContext = React.createContext<AppContextValue | undefined>(undefined);
 export const AppContextProvider = ({ children }) => {
   const [toast, setToast] = useState<ToastMessage>();
 
+  const { isError } = useQuery("validateToken", apiClient.validateToken, { retry: false });
+
   return (
     <AppContext.Provider
       value={{
         showToast: (toast) => setToast(toast),
+        isLoggedIn: !isError,
       }}
     >
       {toast && (
