f0092548825d66c40bf1b25c663ff3f759ebb0f2
Guilherme Miranda <guilherme_de_oliveira_n1@hotmail.com>
2024-03-11 19:40:23 -0300

    feat (backend): add endpoint for hotel creation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,19 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
-import path from "path";
+import myHotelRoutes from "./routes/my-hotels";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 mongoose.connect(process.env.MONGODB_CONNECTION as string);
 
@@ -25,6 +33,7 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
 
 app.listen(7000, () => {
   console.log("server running on localhost:7000");
